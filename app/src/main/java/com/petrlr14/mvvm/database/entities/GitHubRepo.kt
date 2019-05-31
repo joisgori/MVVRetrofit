ddb896d3ac2701eeaaa3dc563d368449f12b8d08
joisgori <37356528+joisgori@users.noreply.github.com>
2019-05-31 10:29:18 -0600

    Actualizadovg
    sdfg

--- app/src/main/java/com/petrlr14/mvvm/database/entities/GitHubRepo.kt
+++ app/src/main/java/com/petrlr14/mvvm/database/entities/GitHubRepo.kt
@@ -6,9 +6,15 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "repos")
 data class GitHubRepo(
+    
     @PrimaryKey
+    @field:Json(name="id")
     val id: Long,
+    
+    @field:Json(name="name")
     val name: String,
+    
+    @field:Json(name="full_name") //no sé si esto debe ir en la línea 17 y lo de la 18 en la 17
     @ColumnInfo(name="full_name")
     val fullName:String
 )
